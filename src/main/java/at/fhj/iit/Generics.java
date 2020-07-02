623bfc54c6e6b4756d69027ec339a14b8d0f91cb
SarahGraschl <62704451+SarahGraschl@users.noreply.github.com>
2020-07-02 16:24:15 +0200

    Adding the generics class

---
+++ src/main/java/at/fhj/iit/Generics.java
@@ -0,0 +1,42 @@
+class Generics<T> {
+
+    /**
+     *
+     */
+    private T t;
+
+    /**
+     * default constructor
+     */
+    Generics(){
+
+    }
+
+    /**
+     * creates a generic the the given value t
+     *
+     * @param t
+     */
+    Generics(T t){
+        this.t = t;
+    }
+
+    /**
+     * sets the value for the generic t
+     *
+     * @param t
+     */
+    public void setT(T t){
+        this.t = t;
+    }
+
+    /**
+     * return the value of the generic t
+     *
+     * @return the generic t
+     */
+    public T getT(){
+        return t;
+    }
+
+}
