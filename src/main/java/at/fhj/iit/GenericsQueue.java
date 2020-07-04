7bbcd14550c6d896ca7d9c866f31fba2dda09fa0
DarleneAntonino <62381676+DarleneAntonino@users.noreply.github.com>
2020-07-04 12:39:22 +0200

    Update GenericsQueue.java
    minor changes; for example the Header

--- src/main/java/at/fhj/iit/GenericsQueue.java
+++ src/main/java/at/fhj/iit/GenericsQueue.java
@@ -4,83 +4,81 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-/*
- * Created by Sarah Graschl; 03.07.2020
- * Last changed by Darlene Antonino; 04.07.2020
+/**
  * This class is very similar to the StringQueue class, but it can be used with every datatype,
  * not only with Strings.
+ *
+ * @author Sarah Graschl
+ * @author Darlene Antonino
+ * @since  03.07.2020
+ * @date   04.07.2020
  */
 
-abstract class GenericsQueue<T> implements IQueue<T> {
-
-	//attributes
-	  private List<T> generics = new ArrayList<T>();
-	  private int maxSize = 5;
+public class GenericsQueue<T> implements IQueue<T> {
 
     /**
-     * default constructor
+     * attributes
      */
-    GenericsQueue(){
-    }
+    private int maxSize = 5;
+    private List<T> generics = new ArrayList<T>();
 
     /**
      * creates a generic the the given integer maxsize so change the maxSize
      *
      * @param maxsize
      */
-    GenericsQueue(int maxsize){
-        this.maxSize = maxsize;
+    GenericsQueue(int maxSize) {
+        this.maxSize = maxSize;
     }
-
 
     @Override
     public boolean offer(T obj) {
-      if (generics.size() != maxSize)
-    	  generics.add(obj);
-      else
-        return false;
+        if (generics.size() != maxSize)
+            generics.add(obj);
+        else
+            return false;
 
-      return true;
+        return true;
     }
 
     @Override
     public T poll() {
-      T element = peek();
+        T element = peek();
 
-      if (generics.size() > 0) {
-    	  generics.remove(0);
-      }
+        if (generics.size() > 0) {
+            generics.remove(0);
+        }
 
-      return element;
+        return element;
     }
 
     @Override
     public T remove() {
-      T element = poll();
-      if (element == null)
-        throw new NoSuchElementException("there's no element any more");
+        T element = poll();
+        if (element == null)
+            throw new NoSuchElementException("there's no element any more");
 
-      return element;
+        return element;
     }
 
     @Override
     public T peek() {
-      T element;
-      if (generics.size() > 0)
-        element = generics.get(0);
-      else
-        element = null;
+        T element;
+        if (generics.size() > 0)
+            element = generics.get(0);
+        else
+            element = null;
 
-      return element;
+        return element;
     }
 
     @Override
     public T element() {
-      T element = peek();
-      if (element == null)
-        throw new NoSuchElementException("there's no element any more");
+        T element = peek();
+        if (element == null)
+            throw new NoSuchElementException("there's no element any more");
 
-      return element;
+        return element;
     }
-    
+
 }
